e67b9ef6b0b98fcec2bea7f37e5be795cfe22a69
ojonghwa <zeamp123>
2020-03-01 06:21:17 +0900

    gitignore 등록

--- src/main/java/com/jojoldu/book/ojonghwa/domain/user/User.java
+++ src/main/java/com/jojoldu/book/ojonghwa/domain/user/User.java
@@ -1,11 +1,18 @@
 package com.jojoldu.book.ojonghwa.domain.user;
 
 import com.jojoldu.book.ojonghwa.domain.BaseTimeEntity;
+import com.jojoldu.book.ojonghwa.domain.user.Role;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor
