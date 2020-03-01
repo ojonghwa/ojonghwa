574a6581f49ef170bab342addf84ac14fd0aafd2
ojonghwa <zeamp123>
2020-03-01 15:28:15 +0900

    gitignore 등록

--- src/main/java/com/jojoldu/book/ojonghwa/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/ojonghwa/web/PostsApiController.java
@@ -1,11 +1,20 @@
 package com.jojoldu.book.ojonghwa.web;
 
 import com.jojoldu.book.ojonghwa.service.PostsService;
+import com.jojoldu.book.ojonghwa.web.dto.PostsListResponseDto;
 import com.jojoldu.book.ojonghwa.web.dto.PostsResponseDto;
 import com.jojoldu.book.ojonghwa.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.ojonghwa.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -34,4 +43,8 @@ public class PostsApiController {
         return postsService.findById(id);
     }
 
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
 }
