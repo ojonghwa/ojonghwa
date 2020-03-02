edc0ebe32cec0c4576d3a6643bf92cc381b319de
ojonghwa <zeamp123>
2020-03-02 23:49:33 +0900

    gitignore 등록

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,15 +9,15 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f java | tail -n 1)
+CURRENT_PID=$(pgrep -fl ojonghwa | grep jar | awk '{print $1}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill $CURRENT_PID"
-    kill $CURRENT_PID
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
     sleep 5
 fi
 
